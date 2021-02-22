6bc82756433f1ce4a67e5ed99ebb5d6b06876ce3
krishnakumar34 <58246610+krishnakumar34@users.noreply.github.com>
2021-02-22 21:37:32 +0530

    Delete build directory
    Deleted build

--- build/index.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-exports.__esModule = true;
-var addon_1 = require("./addon");
-var stremio_addon_sdk_1 = require("stremio-addon-sdk");
-exports["default"] = addon_1["default"]().then(function (addonInterface) { return stremio_addon_sdk_1.getRouter(addonInterface); });
